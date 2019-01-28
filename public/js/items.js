43285e4be05da73c65ee43e20f6b14b93cc6884a
Samuel Bines <samuelBines@gmail.com>
2019-01-28 10:13:05 -0800

    fixex object object responses on items

--- public/js/items.js
+++ public/js/items.js
@@ -29,7 +29,7 @@ const items = new Vue({
     },
     orderItem: async function (data) {
       const response = await apiService.post('/api/order', data);
-      alert(response);
+      alert(response.response);
     }
   }
 
